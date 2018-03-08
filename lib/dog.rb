97f2b676efe0098fc7edc7518fc308493457a925
Chris Henry <cdhenry@gmail.com>
2018-03-08 21:18:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,7 +70,7 @@ class Dog
 
   def self.new_from_db(row)
     dog_hash = {:name => row[1], :breed => row[2]}
-    new_dog = self.new(row[0], dog_hash)
+    new_dog = self.new(row[0], row[1], row[2])
   end
 
   def self.find_by_name(name)
