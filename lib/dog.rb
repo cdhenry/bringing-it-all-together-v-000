04c5e622517066a0c1e73be5cb1132cb9afe370b
Chris Henry <cdhenry@gmail.com>
2018-03-08 21:15:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,8 +5,8 @@ class Dog
 
   def initialize(id: nil, name:, breed:)
     @id = id
-    @name = dog_hash[:name]
-    @breed = dog_hash[:breed]
+    @name = name
+    @breed = breed
   end
 
   def self.create_table
